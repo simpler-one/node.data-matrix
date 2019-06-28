147ee3eda808145cf20fd83f2c6b214b933fcad6
komei-work-work <koba.work,work1127@gmail.com>
2019-06-29 01:00:32 +0900

    config, data-type

--- src/data-matrix.ts
+++ src/data-matrix.ts
@@ -2,12 +2,12 @@ import { DataMatrixHeader, DataGroup } from './interfaces';
 
 type ObjPath = string[];
 
-export class DataMatrix {
+export class DataMatrix<T> {
     static build<T>(headers: DataMatrixHeader, ...groups: DataGroup[]): T[] {
 
     }
 
-    private template;
+    private template: T;
 
     private type: DataType<T>;
 
@@ -18,43 +18,45 @@ export class DataMatrix {
         header: DataMatrixHeader, 
         groups: DataGroup[]
     ) {
-        this.type = new DataType(headers);
+        this.type = new DataType(header);
     }
 }
-
-
 
 
 class DataType<T> {
     private template: T;
     private paths: ObjPath[];
 
-    constructor(header: DataMatrixHeader) {
-        this.path = DataType.getPaths(header);
-        this.template = 
-    }
-
+    
     private static getPaths(header: DataMatrixHeader): ObjPath[] {
         const paths: ObjPath[] = [];
 
-        const len = header[0].length;
-        for (let i = 0; i len; i++) {
-            const path: ObjPath = [];
-            for (let depth = 0; depth < header.length; depth++) {
-                if (header[depth].length <= i) {
-                    break;
-                }
-
-                path.push(header[depth][i]);
-            }
-
-            paths.push(path);
+        for (let i = 0; i < header[0].length; i++) {
+            this.setPath(header, i, 0, [], paths);
         }
 
         return paths;
     }
 
-    private static getTemplate(paths: ObjPath[]): void {
+    private static setPath(header: DataMatrixHeader, i: number, depth: number, path: ObjPath, paths: ObjPath[]): void {
+        if (depth >= header.length || header[depth].length <= i) {
+            paths.push(path); // End
+            return;
+        }
+
+        const item = header[depth][i];
+        if (typeof item === 'string') {
+            path.push(item);
+            this.setPath(header, i, depth + 1, path, paths); // Recursive
+        } else {
+            for (const key of item) {
+                const newPath = [...path, key];
+                this.setPath(header, i, depth + 1, newPath, paths); // Recursive
+            }
+        }
+    }
+
+    private static getTemplate<T>(paths: ObjPath[]): T {
         const template: T = {} as undefined; 
         for (const path of paths) {
             const pPath: ObjPath = path.slice(0, path.length - 1);
@@ -62,7 +64,15 @@ class DataType<T> {
                 
             }
         }
+
+        return template;
     }
+
+    constructor(header: DataMatrixHeader) {
+        this.paths = DataType.getPaths(header);
+        this.template = DataType.getTemplate<T>(this.paths);
+    }
+
 }
 
 
