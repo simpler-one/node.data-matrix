e667bd039fa33576a5661d0d2d0bd63231400eac
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-06-28 19:50:44 +0900

    Update data-matrix.ts

--- src/data-matrix.ts
+++ src/data-matrix.ts
@@ -1,5 +1,6 @@
 import { DataMatrixHeader, DataGroup } from './interfaces';
 
+type ObjPath = string[];
 
 export class DataMatrix {
     static build<T>(headers: DataMatrixHeader, ...groups: DataGroup[]): T[] {
@@ -32,12 +33,20 @@ class DataType<T> {
         
     }
 
-    private build(header: DataMatrixHeader) {
+    private static paths(header: DataMatrixHeader): ObjPath[] {
+        const paths: ObjPath[] = [];
+
         const len = header[0].length;
         for (let i = 0; i len; i++) {
+            const path: ObjPath = [];
             for (let depth = 0; depth < header.length; depth++) {
+                path.push(header[depth][i]);
             }
+
+            paths.push(path);
         }
+
+        return paths;
     }
 }
 
